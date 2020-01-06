54eefdcc941a98f02b354e19ce00bbc87ab069ff
Dale Wang <dalinwanglhtz@gmail.com>
2020-01-06 22:17:13 +1100

    started

---
+++ src/com/codewardev/Diamond.java
@@ -0,0 +1,12 @@
+package com.codewardev;
+
+// https://www.codewars.com/kata/give-me-a-diamond/train/java
+
+public class Diamond {
+
+	public static String print(int n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
