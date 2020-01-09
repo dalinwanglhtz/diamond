254415ec08d548b7ee387249d979eb8fd7742d67
Dale Wang <dalinwanglhtz@gmail.com>
2020-01-09 22:32:20 +1100

    refined working cut

--- src/com/codewardev/Diamond.java
+++ src/com/codewardev/Diamond.java
@@ -9,43 +9,29 @@ import java.util.stream.Collectors;
 public class Diamond {
 
 	public static String print(int n) {
-		/*
-		 * System.out.println("  *\n ***\n*****\n ***\n  *\n");
-		 * System.out.println(" *\n***\n *\n");
-		 */
-		
+		// Even and negative numbers don't make good diamonds
 		if(n%2 == 0 || n < 0) return null;
 		
-		String[] lines = new String[n/2];
-		String aLine = "";
-		int numSpaces = n/2;
-		int counter=0;
-		for(int i=1; i<n; i += 2) {
-			for(int k=0; k<numSpaces; k++) {
-				aLine += " ";
-			}
-			for(int j=0; j<i; j++) {
-				aLine += "*";
-			}
-			aLine += "\n";
-			numSpaces--;
-			lines[counter] = aLine;
-			counter++;
-			aLine ="";
+		// Break diamond in two halves, now processing top half
+		int numSpaces = n/2, counter=0;
+		String[] lines = new String[numSpaces];
+		for(int numStars=1; numStars<n; numStars += 2) {
+			lines[counter++] = printChars(numSpaces--, ' ')+printChars(numStars, '*')+"\n";
 		}
 		
+		// Now processing bottom half by reversing the top half
 		String result = Arrays.stream(lines).collect(Collectors.joining());
 		Arrays.sort(lines, Collections.reverseOrder());
-		for(int i=0; i<n; i++) {
-			aLine += "*";
-		}
-		aLine += "\n";
-		result += aLine;
+		result += printChars(n, '*')+"\n";
 		result += Arrays.stream(lines).collect(Collectors.joining());
 		
-		System.out.println(result);
-		
 		return result;
+	}
+
+	private static String printChars(int num, char x) {
+		char[] array = new char[num];
+		Arrays.fill(array, x);
+		return new StringBuilder().append(array).toString();
 	}
 
 }
